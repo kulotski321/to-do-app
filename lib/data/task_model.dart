caa85b92cfa0b5216d855d0d75c4a4a9cd29f906
Angelo Montil <angelocool177@gmail.com>
2022-06-20 14:12:14 +0800

    Add task data model

---
+++ lib/data/task_model.dart
@@ -0,0 +1,40 @@
+import 'package:equatable/equatable.dart';
+
+class Task extends Equatable {
+  final String title;
+  bool? isCompleted;
+
+  Task({
+    required this.title,
+    this.isCompleted,
+  }) {
+    isCompleted = isCompleted ?? false;
+  }
+
+  Task copyWith({
+    String? title,
+    bool? isCompleted,
+  }) {
+    return Task(
+      title: title ?? this.title,
+      isCompleted: isCompleted ?? this.isCompleted,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'isCompleted': isCompleted,
+    };
+  }
+
+  factory Task.fromMap(Map<String, dynamic> map) {
+    return Task(
+      title: map['title'],
+      isCompleted: map['isCompleted'],
+    );
+  }
+
+  @override
+  List<Object?> get props => [title, isCompleted];
+}
