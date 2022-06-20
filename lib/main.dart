ecfca44abce58f70c93fbdac0948616f2f29b55e
Angelo Montil <angelocool177@gmail.com>
2022-06-20 16:04:36 +0800

    Add home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:to_do_app/presentation/pages/all_task_tab.dart';
+import 'package:to_do_app/presentation/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,70 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyStatefulWidget(),
-    );
-  }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  int _selectedIndex = 0;
-  PageController pageController = PageController();
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-    pageController.jumpToPage(index);
-  }
-
-  void _onPageChanged(int page) {
-    setState(() {
-      _selectedIndex = page;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('To Do App'),
-      ),
-      body: PageView(
-        controller: pageController,
-        onPageChanged: _onPageChanged,
-        children: [
-          AllTaskTab(),
-          AllTaskTab(),
-          AllTaskTab(),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'All',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.check),
-            label: 'Complete',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.incomplete_circle),
-            label: 'Incomplete',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.grey,
-        onTap: _onItemTapped,
-      ),
+      home: const HomePage(),
     );
   }
 }
---
+++ lib/presentation/pages/home_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_app/presentation/pages/all_task_tab.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  PageController pageController = PageController();
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    pageController.jumpToPage(index);
+  }
+
+  void _onPageChanged(int page) {
+    setState(() {
+      _selectedIndex = page;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('To Do App'),
+      ),
+      body: PageView(
+        controller: pageController,
+        onPageChanged: _onPageChanged,
+        children: [
+          AllTaskTab(),
+          AllTaskTab(),
+          AllTaskTab(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'All',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.check),
+            label: 'Complete',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.incomplete_circle),
+            label: 'Incomplete',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
