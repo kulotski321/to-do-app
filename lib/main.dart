bc9b289b76e1e695736ce44d54c9cc03d9a27118
Angelo Montil <angelocool177@gmail.com>
2022-06-22 11:47:30 +0800

    Add bloc setup in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:to_do_app/presentation/pages/home_page.dart';
+import 'package:to_do_app/presentation/screens/home_page.dart';
+import 'blocs/bloc_exports.dart';
 
 void main() {
-  runApp(const MyApp());
+  BlocOverrides.runZoned(
+    () => runApp(const MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'To Do App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return BlocProvider(
+      create: (context) => TaskBloc(),
+      child: MaterialApp(
+        title: 'To Do App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const HomePage(),
       ),
-      home: const HomePage(),
     );
   }
 }
