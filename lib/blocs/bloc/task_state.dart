5ce8b42a2b235bcf8ccd1b9bcf1ebc4255f8219c
Angelo Montil <angelocool177@gmail.com>
2022-06-22 20:50:20 +0800

    Add complete and incomplete list in state

--- lib/blocs/bloc/task_state.dart
+++ lib/blocs/bloc/task_state.dart
@@ -2,11 +2,15 @@ part of 'task_bloc.dart';
 
 class TaskState extends Equatable {
   final List<Task> allTasks;
+  final List<Task> completedTasks;
+  final List<Task> incompletetasks;
 
   const TaskState({
     this.allTasks = const <Task>[],
+    this.completedTasks = const <Task>[],
+    this.incompletetasks = const <Task>[],
   });
 
   @override
-  List<Object> get props => [allTasks];
+  List<Object> get props => [allTasks, completedTasks, incompletetasks];
 }
