3cbed58122a67ba9497aa5079e022130c022d4a6
Angelo Montil <angelocool177@gmail.com>
2022-06-23 12:18:12 +0800

    Add task bloc tests

--- test/blocs/task/task_bloc_test.dart
+++ test/blocs/task/task_bloc_test.dart
@@ -1,14 +1,73 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:to_do_app/blocs/bloc_exports.dart';
+import 'package:to_do_app/data/task_model.dart';
 
 void main() {
-  TaskBloc taskBloc = TaskBloc();
+  TaskBloc buildBloc() {
+    return TaskBloc();
+  }
 
-  tearDown(() {
-    taskBloc.close();
+  group('constructor', () {
+    test('works properly', () {
+      expect(buildBloc, returnsNormally);
+    });
+
+    test('has correct initial state', () {
+      expect(buildBloc().state, equals(const TaskState()));
+    });
   });
 
-  test('TaskBloc should have initial state as [TaskState]', () {
-    expect(taskBloc.state.runtimeType, TaskState);
+  group('AddTask', () {
+    final mockTask = Task(id: 'id', title: 'title', isCompleted: false);
+    blocTest<TaskBloc, TaskState>('emits new state with the added task',
+        build: buildBloc,
+        act: (bloc) => bloc.add(AddTask(task: mockTask)),
+        expect: () => [
+              TaskState(
+                allTasks: [mockTask],
+                incompletetasks: [mockTask],
+                completedTasks: const [],
+              )
+            ]);
+  });
+
+  group('UpdateTask', () {
+    final mockTask = Task(id: 'id', title: 'title', isCompleted: false);
+    final updatedTask = Task(id: 'id', title: 'title', isCompleted: true);
+    blocTest<TaskBloc, TaskState>('emits new state with the updated task',
+        build: buildBloc,
+        seed: () => TaskState(
+              allTasks: [mockTask],
+              incompletetasks: [mockTask],
+              completedTasks: const [],
+            ),
+        act: (bloc) => bloc.add(UpdateTask(task: mockTask)),
+        expect: () => [
+              TaskState(
+                allTasks: [updatedTask],
+                incompletetasks: const [],
+                completedTasks: [updatedTask],
+              )
+            ]);
+  });
+
+  group('DeleteTask', () {
+    final mockTask = Task(id: 'id', title: 'title', isCompleted: false);
+    blocTest<TaskBloc, TaskState>('emits new state with the deleted task',
+        build: buildBloc,
+        seed: () => TaskState(
+              allTasks: [mockTask],
+              incompletetasks: [mockTask],
+              completedTasks: const [],
+            ),
+        act: (bloc) => bloc.add(DeleteTask(task: mockTask)),
+        expect: () => [
+              const TaskState(
+                allTasks: [],
+                incompletetasks: [],
+                completedTasks: [],
+              )
+            ]);
   });
 }
